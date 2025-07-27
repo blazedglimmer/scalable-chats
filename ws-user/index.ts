9818d541dacc3eac53cc89eb8cbe6aec0509c39d
alphacoder-mp3 <shindeakshay223@gmail.com>
2025-07-27 12:13:53 +0530

    Write websocket logic for user join, leave, message and ping

--- ws-user/index.ts
+++ ws-user/index.ts
@@ -14,6 +14,47 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
+
+    const message = JSON.parse(data.toString());
+    if (message.type === 'join') {
+      const roomName = message.room || 'default';
+      if (!rooms[roomName]) {
+        rooms[roomName] = { sockets: [] };
+      }
+      rooms[roomName].sockets.push(ws);
+      ws.send(JSON.stringify({ type: 'joined', room: roomName }));
+    }
+
+    if (message.type === 'leave') {
+      const roomName = message.room || 'default';
+      if (rooms[roomName]) {
+        rooms[roomName].sockets = rooms[roomName].sockets.filter(
+          socket => socket !== ws
+        );
+        ws.send(JSON.stringify({ type: 'left', room: roomName }));
+      }
+    }
+
+    if (message.type === 'message') {
+      const roomName = message.room || 'default';
+      if (rooms[roomName]) {
+        rooms[roomName].sockets.forEach(socket => {
+          if (socket !== ws && socket.readyState === WebSocket.OPEN) {
+            socket.send(
+              JSON.stringify({
+                type: 'message',
+                room: roomName,
+                content: message.content,
+              })
+            );
+          }
+        });
+      }
+    }
+
+    if (message.type === 'ping') {
+      ws.send(JSON.stringify({ type: 'pong' }));
+    }
   });
 
   ws.send('something');
